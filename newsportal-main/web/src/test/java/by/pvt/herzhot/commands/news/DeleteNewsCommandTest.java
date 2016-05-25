e3f3a0ca33b061cbe78ca5556a3e7b3f42bb6810
Alexander Herzhot <zeon2007@yandex.ru>
2016-05-25 10:40:49 +0300

    add DeleteNewsCommandTest class

---
+++ newsportal-main/web/src/test/java/by/pvt/herzhot/commands/news/DeleteNewsCommandTest.java
@@ -0,0 +1,24 @@
+package by.pvt.herzhot.commands.news;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Herzhot
+ * @version 1.0
+ *          25.05.2016
+ */
+public class DeleteNewsCommandTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
