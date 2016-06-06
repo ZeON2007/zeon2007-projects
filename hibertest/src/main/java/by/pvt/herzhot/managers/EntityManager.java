6035878518ab17844b8fc9368f955588335efcac
Alexander Herzhot <zeon2007@yandex.ru>
2016-06-06 17:44:59 +0300

    entity field constructor (v2)

--- hibertest/src/main/java/by/pvt/herzhot/managers/EntityManager.java
+++ hibertest/src/main/java/by/pvt/herzhot/managers/EntityManager.java
@@ -8,6 +8,9 @@ import by.pvt.herzhot.pojos.Entity;
 import by.pvt.herzhot.utils.InputValueValidator;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import static java.lang.System.in;
@@ -23,49 +26,29 @@ public enum EntityManager {
     INSTANCE;
 
     private Scanner scanner = new Scanner(in);
+    private List<Field> allFields = new ArrayList<>();
+    private Entity instance = null;
+    private Class<?> clazz = null;
 
     public Entity buildEntity(final Entity entity) {
-        if (entity instanceof Author) {
-
-            Entity instance = null;
-            Class<?> clazz = entity.getClass();
+            clazz = entity.getClass();
             try {
                 instance = (Entity) clazz.newInstance();
-                Field[] fields = clazz.getDeclaredFields();
-                for (Field field : fields) {
+                while (clazz != Object.class) {
+                    allFields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+                    clazz = clazz.getSuperclass();
+                }
+                for (Field field : allFields) {
                     field.setAccessible(true);
                     if (!field.getName().equals("serialVersionUID")) {
                         out.println("Input " + field.getName() + ":");
                         field.set(instance, InputValueValidator.INSTANCE.validate(field.getType()));
                     }
                 }
-            } catch (Exception e) {}
+            } catch (Exception e) {
+                out.println("Build entity error!");
 
+            }
             return instance;
-        } else if (entity instanceof CategoryOfNews) {
-            CategoryOfNews categoryOfNews = new CategoryOfNews();
-            categoryOfNews.setId(assignId());
-            out.println("Please enter category name:");
-            categoryOfNews.setCategory(scanner.nextLine().trim());
-            return categoryOfNews;
-
-        } else if (entity instanceof CreditCard) {
-            return new CreditCard();
-        } else if (entity instanceof BankAccount) {
-            return new BankAccount();
-        } else return null;
-    }
-    private int assignId() {
-        int id;
-        try {
-            out.println("Please enter id:");
-            id  = Integer.valueOf(scanner.nextLine().trim());
-        }
-        catch (NullPointerException | NumberFormatException e) {
-            out.println("You enter not number!");
-            out.println("Id was defined as 0");
-            id = 0;
-        }
-        return id;
     }
 }
