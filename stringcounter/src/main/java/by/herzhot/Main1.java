ce378629a7670da4029d19b2a21ae1a2f55aff4d
Alexander Herzhot <zeon2007@yandex.ru>
2018-08-06 09:52:50 +0300

    test

--- stringcounter/src/main/java/by/herzhot/Main1.java
+++ stringcounter/src/main/java/by/herzhot/Main1.java
@@ -10,14 +10,21 @@ public class Main1 {
 
         String s1 = "a";
 
-        String s2 = "ab";
+        String s2 = "aa";
 
         String s3 = "aaa";
 
-        String s4 = "abaa";
+        String s4 = "aaaa";
+
+        String s26 = "abcdefghijklmnopqrstuvwxyz";
+
+        String s250 = "baaafhskfjhsgkghghhgghghghghdsdhjjjhhhdjhdjhdjhhhgskjdhgjdhggfslflskfjfjjjjjjffffffffffffffffaaafhskfjhsgkghghhgghghghghdsdhjjjhhhdjhdjhdjhhhgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffffffffffffhgghghghghgskjdhhgghghghghgskjdhgjdhggfslflskfjfjjjjjjff";
+
+        String s500 = "baaafhskfjhsgkghghhgghghghghdsdhjjjhhhdjhdjhdjhhhgskjdhgjdhggfslflskfjfjjjjjjffffffffffffffffaaafhskfjhsgkghghhgghghghghdsdhjjjhhhdjhdjhdjhhhgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffffffffffffhgghghghghgskjdhhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfsl";
 
         String s1000 = "baaafhskfjhsgkghghhgghghghghdsdhjjjhhhdjhdjhdjhhhgskjdhgjdhggfslflskfjfjjjjjjffffffffffffffffaaafhskfjhsgkghghhgghghghghdsdhjjjhhhdjhdjhdjhhhgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffffffffffffhgghghghghgskjdhhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjffffffffff";
 
+        String s1500 = s1000 + s500;
 
         String s2000 = "baaafhskfjhsgkghghhgghghghghdsdhjjjhhhdjhdjhdjhhhgskjdhgjdhggfslflskfjfjjjjjjffffffffffffffffaaafhskfjhsgkghghhgghghghghdsdhjjjhhhdjhdjhdjhhhgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffffffffffffhgghghghghgskjdhhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffffffffffffffffffjjfjfjfjfjdfjhfhfhfhfhfhfhfhfhfhfdddddddddddddddfhdfjhffhfhfdfdfdfdfdfdfdfdfdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflffffffhgghghghghgskjdhgjdhgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjd";
 
@@ -33,7 +40,7 @@ public class Main1 {
                 + "baaafhskfjhsgkghghhgghghghghdsdhjjjhhhdjhdjhdjhhhgskjdhgjdhggfslflskfjfjjjjjjffffffffffffffffaaafhskfjhsgkghghhgghghghghdsdhjjjhhhdjhdjhdjhhhgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffffffffffffhgghghghghgskjdhhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjfffffffffffffffhgghghghghgskjdhgjdhggfslflskfjfjjjjjjffffffffff";
 
 
-        String s = s3;
+        String s = s3000;
         System.out.println((int)'a');
         System.out.println("Length is: " + s.length());
         int[][] queries = new int[][]{
@@ -41,12 +48,93 @@ public class Main1 {
         };
 
         long t10 = System.currentTimeMillis();
-        System.out.println(Arrays.toString(countSubstrings(s, queries)) + " | " + (System.currentTimeMillis() - t10));
+        System.out.println(Arrays.toString(countSubstrings1(s, queries)) + " | " + (System.currentTimeMillis() - t10));
         t10 = System.currentTimeMillis();
         System.out.println(Arrays.toString(countSubstringsFastWithHashes(s, queries)) + " | " + (System.currentTimeMillis() - t10));
-        t10 = System.currentTimeMillis();
-        System.out.println(Arrays.toString(countSubstringsAccurateAndSlow(s, queries)) + " | " + (System.currentTimeMillis() - t10));
+//        t10 = System.currentTimeMillis();
+//        System.out.println(Arrays.toString(countSubstringsAccurateAndSlow(s, queries)) + " | " + (System.currentTimeMillis() - t10));
 
+    }
+
+    private static int[] countSubstrings1(String s, int[][] queries) {
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++ ) {
+            int offset = queries[i][0];
+            int ssLength = queries[i][1] - offset + 1;
+            if (ssLength == 1) {
+                result[i] = 1;
+            } else {
+                int tailsCount = 0;
+                int groupCount = 0;
+                int[] chars = new int[ssLength];
+                int[][] mapping = new int[227782][26];
+                int[] posToGroup = new int[ssLength];
+                int[] groupToAmount = new int[227782];
+                int[] from = new int[ssLength];
+                int[] to = new int[ssLength];
+
+                for (int j = 0; j < ssLength; j++) {
+                    chars[j] = s.charAt(offset + j) - 97;
+                    if (mapping[0][chars[j]] != 0) {
+                        posToGroup[j] = mapping[0][chars[j]];
+                        groupToAmount[posToGroup[j]]++;
+                        if (j != ssLength - 1) {
+                            from[j + 1] = j;
+                            to[j] = j + 1;
+                        } else {
+                            from[0] = j;
+                            to[j] = 0;
+                        }
+                    } else {
+                        mapping[0][chars[j]] = ++groupCount;
+                        posToGroup[j] = groupCount;
+                        groupToAmount[groupCount]++;
+                        if (j != ssLength - 1) {
+                            from[j + 1] = j;
+                            to[j] = j + 1;
+                        } else {
+                            from[0] = j;
+                            to[j] = 0;
+                        }
+                    }
+                }
+
+                int curLen = 1;
+                boolean isEnd = false;
+                for (int k = 0; ; k = to[k]) {
+                    if (to[k] == k) {
+                        isEnd = true;
+                    }
+                    if (groupToAmount[posToGroup[k]] == 1) {
+                        tailsCount += ssLength - k - curLen;
+                        to[from[k]] = to[k];
+                        from[to[k]] = from[k];
+                        if (k + curLen >= ssLength) {
+                            curLen++;
+                        }
+                    } else {
+                        if (k + curLen < ssLength) {
+                            if (mapping[posToGroup[k]][chars[k + curLen]] != 0) {
+                                posToGroup[k] = mapping[posToGroup[k]][chars[k + curLen]];
+                                groupToAmount[posToGroup[k]]++;
+                            } else {
+                                mapping[posToGroup[k]][chars[k + curLen]] = ++groupCount;
+                                posToGroup[k] = groupCount;
+                                groupToAmount[groupCount]++;
+                            }
+                        } else {
+                            curLen++;
+                            to[from[k]] = to[k];
+                            from[to[k]] = from[k];
+                        }
+                    }
+                    if (isEnd) break;
+                }
+                System.out.println(groupCount);
+                result[i] = groupCount + tailsCount;
+            }
+        }
+        return result;
     }
 
     private static int[] countSubstrings(String s, int[][] queries) {
@@ -146,7 +234,8 @@ public class Main1 {
             } else {
                 int counter = 0;
                 int counterHashes = 0;
-                int size = 4501500;
+//                int size = 4501500;
+                int size = 114501500;
                 int[] hashes = new int[size];
                 byte[] subStr = new byte[queries[i][1] - queries[i][0] + 1];
                 s.getBytes(queries[i][0],queries[i][1] + 1, subStr, 0);
